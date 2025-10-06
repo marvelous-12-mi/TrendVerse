693c682a2c58e3e49ca22f51cd26e421d69e56f0
marvelous-12-mi <134082084+marvelous-12-mi@users.noreply.github.com>
2025-10-06 15:01:02 -0400

    Add files via upload

---
+++ app/(app)/page.jsx
@@ -0,0 +1,29 @@
+import {  getMyPostsFeed } from "@/actions/post";
+import HomeView from "@/sections/home/view/HomeView";
+import { currentUser } from "@clerk/nextjs";
+import { QueryClient } from "@tanstack/react-query";
+
+export const metadata = () => {
+  return {
+    title: `Vibin`,
+    description: `Where moments become memories`,
+  };
+};
+
+const HomePage = async () => {
+  // const queryClient = new QueryClient();
+  // const user = await currentUser()
+  // // get posts
+  // await queryClient.prefetchInfiniteQuery({
+  //   queryKey: ["posts", "all"],
+  //   queryFn: ({ pageParam = "" }) => getMyPostsFeed(pageParam),
+  //   getNextPageParam: (lastPage) => {
+  //     return lastPage?.metaData.lastCursor;
+  //   },
+  //   enabled: !!user,
+  // });
+
+  return <HomeView />;
+};
+
+export default HomePage;
---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import css from "@/styles/authLayout.module.css";
+import Image from "next/image";
+export const metadata = {
+  title: "Authentication",
+};
+const AuthLayout = ({ children }) => {
+  return (
+    <div className={css.wrapper}>
+      <div className={css.container}>
+        <div className={css.left}>{children}</div>
+        <div className={css.right}>
+          <Image
+            src="/images/auth.png"
+            alt="branding-image"
+            quality={100}
+            width={400}
+            height={480}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
